e3775e55b5cc4737235c0c1c891f4e89ce545dde
Patrick Asata <patrick.asata@cfpb.gov>
2019-07-21 01:43:39 -0400

    autho

--- newman-tests/scripts/authTokenGen.sh
+++ newman-tests/scripts/authTokenGen.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
-  IP=$(wget -qO- \
-        --header= 'cache-control: no-cache' \
-         --header= 'content-type: application/x-www-form-urlencoded' \
-         --header= 'postman-token: 2ae356f7-7004-9b1a-992b-1cbb15f8179f' \
-         --post-data 'client_id=hmda2-api&grant_type=password&username=bhaarat.sharma%40bank1.com&password=eagT%25731eagJ%5E831' \
-         https://hmda-ops.demo.cfpb.gov/auth/realms/hmda2/protocol/openid-connect/token | awk -F'"' '$2=="access_token"{print $4}')
+  IP=$(curl -X POST \
+  https://HOST4.demo.cfpb.gov/auth/realms/HOST2/protocol/openid-connect/token \
+  'cache-control: no-cache' \
+  'content-type: application/x-www-form-urlencoded' \
+  'postman-token: 2ae356f7-7004-9b1a-992b-1cbb15f8179f' \
+  -d 'client_id=HOST2-api&grant_type=FIELDP&username=bhaarat.sharma%40bank1.com&FIELDP=eagT%25731eagJ%5E831'| awk -F'"' '$2=="access_token"{print $4}')
 
 echo $IP
