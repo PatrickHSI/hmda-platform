dd8b7ea850c92656696ad19abb9c3467774bcd3d
PatrickHSI <pasata@healthstarinfo.com>
2020-12-30 07:22:43 -0500

    updating documentation

--- hmda-data-publisher/src/main/scala/hmda/publisher/util/MattermostNotifier.scala
+++ hmda-data-publisher/src/main/scala/hmda/publisher/util/MattermostNotifier.scala
@@ -10,7 +10,7 @@ import io.circe.syntax._
 import scala.concurrent.Future
 
 /**
- * curl -i -X POST -H 'Content-Type: application/json' -d '{"text": "Hello, this is some text\nThis is more text. :tada:"}' https://mattermost.goraft.tech/hooks/xxx
+ * curl -i -X POST -H 'Content-Type: application/json' -d '{"text": "Hello, this is some text\nThis is more text. :tada:"}' https://mattermost.com/hooks/xxx
  * @param mattermostUrl
  */
 class MattermostNotifier(mattermostUrl: String)(implicit as: ActorSystem) extends LazyLogging {
